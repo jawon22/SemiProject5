d9146103921195fac6ce89a1684e6de17d5a5170
jawon22 <winwkdnjs@naver.com>
2023-08-30 09:52:58 +0900

    테스트용 프로젝트입니다(연습해보기)

---
+++ testsemiproject/src/main/java/com/kh/springhome/TestsemiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.springhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsemiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsemiprojectApplication.class, args);
+	}
+
+}
