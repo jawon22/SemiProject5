d305d3ffe0f385df1bcdc148843e8f2f5acf4378
jawon22 <winwkdnjs@naver.com>
2023-09-17 23:58:36 +0900

    찐프로젝트 준비 완료

---
+++ realSemiProject/src/main/java/com/semi/project/RealSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.semi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealSemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealSemiProjectApplication.class, args);
+	}
+
+}
