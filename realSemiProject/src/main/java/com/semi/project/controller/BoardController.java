764fe72d54ab6b4de5277b30a392dd0ba23ee225
jawon22 <winwkdnjs@naver.com>
2023-09-19 14:19:42 +0900

    jsp 수정 및 boardlist 구현중

--- realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
+++ realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.semi.project.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,12 +13,17 @@ import com.semi.project.vo.PaginationVO;
 @RequestMapping("/board")
 public class BoardController {
 	
-//	@RequestMapping("/list")
+//	@RequestMapping("/list") // 정보게시판 리스트
 //	public String list(@ModelAttribute(name="vo") PaginationVO vo, Model model) {
-//		
+		
 //		int count = vo.isSearch() ? boardDao.countList(vo) : boardDao.countList();
+//		vo.setCount(count);
 //		
+//		List<BoardDto> list =boardDao.selectListByPage(vo);
+//		model.addAttribute("list",list);
 //		
+//		return "/WEB-INF/views/board/list2.jsp";
+		
 //	}
 
 }
