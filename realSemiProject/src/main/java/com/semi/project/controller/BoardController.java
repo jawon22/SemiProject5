9d080003419309641b5327dd62842deac9d84f8e
Havenlife <taeseok511@gmail.com>
2023-09-19 18:06:04 +0900

    화면 구현을 위한 컨트롤 기능 구현

--- realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
+++ realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
@@ -2,29 +2,36 @@ package com.semi.project.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.semi.project.dao.BoardDao;
+import com.semi.project.dto.BoardDto;
 import com.semi.project.vo.PaginationVO;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	BoardDao boardDao;
 	
-//	@RequestMapping("/list") // 정보게시판 리스트
-//	public String list(@ModelAttribute(name="vo") PaginationVO vo, Model model) {
+	@RequestMapping("/list") // 정보게시판 리스트
+	public String list(Model model, BoardDto boardDto) {
+		List<BoardDto> list = boardDao.selectList();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/board/list.jsp";
+	}
+	
+	
+	@RequestMapping("/detail")
+	public String detail(@RequestParam int boardNo, Model model) {
 		
-//		int count = vo.isSearch() ? boardDao.countList(vo) : boardDao.countList();
-//		vo.setCount(count);
-//		
-//		List<BoardDto> list =boardDao.selectListByPage(vo);
-//		model.addAttribute("list",list);
-//		
-//		return "/WEB-INF/views/board/list2.jsp";
-		
-//	}
+		return "/WEB-INF/views/board/detail.jsp";
+	}
 
 }
 
