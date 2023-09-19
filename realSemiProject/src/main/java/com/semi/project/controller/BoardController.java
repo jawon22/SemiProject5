2fa5e0b0d12dc32dd78810c1f90be18a4d0cf913
jawon22 <winwkdnjs@naver.com>
2023-09-19 13:57:34 +0900

    다른 조원이 만든 것들 받아오기 위한 커밋

---
+++ realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
@@ -0,0 +1,39 @@
+package com.semi.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.semi.project.vo.PaginationVO;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+//	@RequestMapping("/list")
+//	public String list(@ModelAttribute(name="vo") PaginationVO vo, Model model) {
+//		
+//		int count = vo.isSearch() ? boardDao.countList(vo) : boardDao.countList();
+//		
+//		
+//	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ realSemiProject/src/main/java/com/semi/project/vo/PaginationVO.java
@@ -0,0 +1,74 @@
+package com.semi.project.vo;
+
+import lombok.Data;
+
+@Data
+public class PaginationVO {
+	private String type, keyword; //검색 분류 및 키워드
+	private int page =1; //현재 페이지 번호
+	private int size =10; // 보유줄 게시글 수 ->기본값 10
+	
+	private int count; // 전체 글 수 (DB에서 구해와야하는 값)
+	private int navigatorSize=10; // 하단 네비게이터 표시 개수(기본값 10)
+	
+	public boolean isSearch() {
+		return type !=null && keyword !=null;
+	}
+	
+	public int getBegin() {
+		return (page-1)/navigatorSize*navigatorSize+1;
+	}
+	public int getEnd() {
+		int end = getBegin()+navigatorSize-1;
+		return Math.min(getPageCount(), end);
+	}
+	
+	public boolean isFirst() {
+		return getBegin()==1;
+	}
+	
+	public String getPrevQueryString() {
+		if(isSearch()) {
+			return "page="+(getBegin()-1)+"&size="+size+"&type="+type+"&keyword="+keyword;
+			}
+		else {//목록
+			return "page="+(getBegin()-1)+"&size="+size;
+		}
+	}
+	
+	public int getPageCount() {
+		return (count-1)/size+1;
+	}
+	
+	public boolean isLast() {
+		return getEnd() >= getPageCount();
+	}
+	
+	public String getNextQueryString() {
+		if(isSearch()) {
+			return "page="+(getEnd()+1)+"&size="+size+"&type="+type+"&keyword="+keyword;
+			}
+		else {//목록
+			return "page="+(getEnd()+1)+"&size="+size;
+		}
+	}
+	
+	public String getQueryString(int page) {
+		if(isSearch()) {
+			return "page="+page+"&size="+size+"&type="+type+"&keyword="+keyword;
+			}
+		else {//목록
+			return "page="+page+"&size="+size;
+		}
+	}
+	
+	public int getStartRow() {
+		return getFinishRow() - (size-1);
+	}
+	public int getFinishRow() {
+		return page* size;
+	}
+	
+	
+	
+}
