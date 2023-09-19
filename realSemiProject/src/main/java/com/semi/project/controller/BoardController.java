7d4f4b9f2ae8c1646d7238b0f185d8a761420110
jawon22 <winwkdnjs@naver.com>
2023-09-19 23:07:04 +0900

    정보게시판 구현
    board_list ListDto, ListMapper 생성
    list.jsp구현

--- realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
+++ realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.semi.project.dao.BoardDao;
-import com.semi.project.dto.BoardDto;
+import com.semi.project.dto.BoardListDto;
 import com.semi.project.vo.PaginationVO;
 
 @Controller
@@ -25,7 +25,7 @@ public class BoardController {
 //		int count = vo.isSearch() ? boardDao.countList(vo) : boardDao.countList();
 //		vo.setCount(count);
 		
-		List<BoardDto> list =boardDao.selectListByPage(vo);
+		List<BoardListDto> list =boardDao.selectListByPage(vo);
 		model.addAttribute("list",list);
 		
 		return "/WEB-INF/views/board/list.jsp";
--- realSemiProject/src/main/java/com/semi/project/dto/BoardDto.java
+++ realSemiProject/src/main/java/com/semi/project/dto/BoardDto.java
@@ -1,12 +1,6 @@
 package com.semi.project.dto;
 
 import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 import lombok.Data;
 
 @Data
@@ -20,24 +14,5 @@ public class BoardDto {
 	long boardReadcount;
 	long boardReplycount;
 	long boardLikecount;
-	
-	
-	
-	//날짜에 따라 다른 값을 반환하는 메소드
-		public String getBoardCtimeString() {
-			LocalDate curent = LocalDate.now(); // 현재날짜
-			LocalDate ctime = boardCtime.toLocalDate();// 작성일
-			
-			if(ctime.equals(curent)) { //작성일 == 오늘
-				Timestamp stamp = new Timestamp(boardCtime.getTime());
-				LocalDateTime time = stamp.toLocalDateTime();
-				LocalTime result = time.toLocalTime();
-				return result.format(DateTimeFormatter.ofPattern("HH:mm"));
-			}
-			else {
-				return ctime.toString();
-			}
-			
-		}
 	
 }
