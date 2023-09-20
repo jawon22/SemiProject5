312c2aeb04df21d0e260331aee43ed7a075843cc
ph2tiger <ph2tiger@naver.com>
2023-09-20 14:15:14 +0900

    board컨트롤러 작성 기능 수정중

--- realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
+++ realSemiProject/src/main/java/com/semi/project/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.semi.project.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -44,8 +46,42 @@ public class BoardController {
 		return "/WEB-INF/views/board/detail.jsp";
 	}
 
-	@GetMapping("/write")
-	//@PostMapping("/write")
+    @GetMapping("/write")
+    public String write(Model model, 
+                        @RequestParam(required = false) Integer boardParent) {
+        // 답글이라면 원본글 정보를 화면에 전달
+        if (boardParent != null) {
+            BoardDto originDto = boardDao.selectOne(boardParent);
+            model.addAttribute("originDto", originDto);
+            model.addAttribute("isReply", true);
+        } else {
+            model.addAttribute("isReply", false);
+        }
+        return "/WEB-INF/views/board/write.jsp";
+    }
+
+    @PostMapping("/write")
+    public String write(@ModelAttribute BoardDto boardDto, HttpSession session,
+                        @RequestParam String boardCategory,
+                        @RequestParam String boardTitle,
+                        @RequestParam String boardContent) {
+        int boardNo = boardDao.sequence();
+        boardDto.setBoardNo(boardNo);
+        String memberId = (String) session.getAttribute("name");
+        boardDto.setBoardWriter(memberId);
+        
+        // boardCategory, boardTitle, boardContent 설정
+        int category = Integer.parseInt(boardCategory);
+        boardDto.setBoardCategory(category);
+        boardDto.setBoardTitle(boardTitle);
+        boardDto.setBoardContent(boardContent);
+
+        // 글을 등록
+        boardDao.insert(boardDto);
+
+        return "redirect:detail?boardNo=" + boardNo;
+    }
+
 	//@RequestMapping("/delete")
 	//@GetMapping("/edit")
 	//@PostMapping("/edit")
