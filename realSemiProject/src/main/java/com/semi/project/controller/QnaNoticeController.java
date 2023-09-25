ccd09206e666742e29a342c5351acd649e115b34
user7198548 <eastsluv@naver.com>
2023-09-25 18:26:03 +0900

    Q&A 게시판 목록 구현 중

--- realSemiProject/src/main/java/com/semi/project/controller/QnaNoticeController.java
+++ realSemiProject/src/main/java/com/semi/project/controller/QnaNoticeController.java
@@ -3,8 +3,8 @@ package com.semi.project.controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
@@ -23,11 +23,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.semi.project.configuration.FileUploadProperties;
 import com.semi.project.dao.AttachmentDao;
 import com.semi.project.dao.QnaNoticeDao;
 import com.semi.project.dto.AttachmentDto;
 import com.semi.project.dto.QnaNoticeDto;
+import com.semi.project.vo.PaginationVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -46,7 +46,7 @@ public class QnaNoticeController {
 	public String write(Model model, 
 				@RequestParam(required = false) Integer qnaNoticeParent) {
 		
-		if(qnaNoticeParent != null) {
+		if(qnaNoticeParent != null) { 
 			QnaNoticeDto originDto = qnaNoticeDao.selectOne(qnaNoticeParent);
 			model.addAttribute("originDto", originDto); 
 			model.addAttribute("isReply", true);
@@ -199,4 +199,32 @@ public class QnaNoticeController {
 		return "/WEB-INF/views/qnaNotice/detail.jsp";
 	}
 	
+	@RequestMapping("/list")
+	public String list(@ModelAttribute(name="vo") PaginationVO vo,
+			Model model) {
+		
+	    int count = qnaNoticeDao.countList(vo);
+	    vo.setCount(count);
+
+		
+		//첫번째페이지에 공지글 5개만 보여줌
+		List<QnaNoticeDto> noticeListTop5 = qnaNoticeDao.selectNoticeListTop5();
+		model.addAttribute("noticeListTop5", noticeListTop5);
+
+		//첫번째페이지부터 목록을 보여줌(공지글 5개 밑에)
+		List<QnaNoticeDto> qnaList = qnaNoticeDao.selectQnaListByPage(vo);
+		model.addAttribute("qnaList", qnaList);
+		
+		return "/WEB-INF/views/qnaNotice/list.jsp";
+	}
+	
+	@RequestMapping("/noticeList")
+	public String noticeList(@ModelAttribute(name="vo") PaginationVO vo,
+			Model model) {
+		//공지보기 선택했을 때 공지 전체목록을 보여줌
+		List<QnaNoticeDto> noticeList = qnaNoticeDao.selectNoticeListByPage(vo);
+		model.addAttribute("noticeList", noticeList);
+		
+		return "/WEB-INF/views/qnaNotice/noticeList.jsp";
+	}
 }
--- realSemiProject/src/main/java/com/semi/project/dao/QnaNoticeDao.java
+++ realSemiProject/src/main/java/com/semi/project/dao/QnaNoticeDao.java
@@ -1,7 +1,10 @@
 package com.semi.project.dao;
 
+import java.util.List;
+
 import com.semi.project.dto.AttachmentDto;
 import com.semi.project.dto.QnaNoticeDto;
+import com.semi.project.vo.PaginationVO;
 
 public interface QnaNoticeDao {
 	
@@ -10,4 +13,10 @@ public interface QnaNoticeDao {
 	void connect(int attachmentNo, int qnaNoticeNo);
 	QnaNoticeDto selectOne(int qnaNoticeNo);
 	AttachmentDto findImage(int qnaNoticeNo);
+	//
+	int countList(PaginationVO vo);
+	List<QnaNoticeDto> selectNoticeListTop5();
+	List<QnaNoticeDto> selectNoticeListByPage(PaginationVO vo);
+	List<QnaNoticeDto> selectQnaListByPage(PaginationVO vo);
+	
 }
